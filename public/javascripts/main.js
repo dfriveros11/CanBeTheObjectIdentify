c8cd6e894b752dd8161f092b4c55953418ddf7f2
Diego Riveros <df.riveros11@uniandes.edu.co>
2020-02-29 20:44:41 -0500

    ahi vamos

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -92,8 +92,14 @@ async function getVideo() {
       };
     });
 
-    //LOADING COCO
+    //Show to the user that is loading the coco
     loadImage();
+
+    this.videoElement.setAttribute("ref", "{this.videoElement}");
+    const div = document.getElementById("root");
+    this.snapShotCanvas.setAttribute("class", "showvideo");
+    this.snapShotCanvas.setAttribute("ref", "{this.snapShotCanvas}");
+    div.appendChild(this.snapShotCanvas);
 
     // define a Promise that'll be used to load the model
     const loadlModelPromise = await cocoSsd.load();
@@ -101,7 +107,7 @@ async function getVideo() {
     // resolve all the Promises
     Promise.all([loadlModelPromise, streamPromise])
       .then(values => {
-        this.detectFromVideoFrame(values[0], this.videoElement);
+        detectFromVideoFrame(values[0], this.videoElement);
       })
       .catch(error => {
         console.error(error);
@@ -119,4 +125,52 @@ function loadImage() {
   div.appendChild(spinner);
   document.getElementById("main").innerHTML = "";
   document.getElementById("main").appendChild(div);
+}
+
+function detectFromVideoFrame(model, video) {
+  model.detect(video).then(
+    predictions => {
+      this.showDetections(predictions);
+
+      requestAnimationFrame(() => {
+        this.detectFromVideoFrame(model, video);
+      });
+    },
+    error => {
+      console.log("Couldn't start the webcam");
+      console.error(error);
+    }
+  );
+}
+
+function showDetections(predictions) {
+  const ctx = this.snapShotCanvas.getContext("2d");
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  const font = "24px helvetica";
+  ctx.font = font;
+  ctx.textBaseline = "top";
+
+  predictions.forEach(prediction => {
+    const x = prediction.bbox[0];
+    const y = prediction.bbox[1];
+    const width = prediction.bbox[2];
+    const height = prediction.bbox[3];
+    // Draw the bounding box.
+    ctx.strokeStyle = "#2fff00";
+    ctx.lineWidth = 1;
+    ctx.strokeRect(x, y, width, height);
+    // Draw the label background.
+    ctx.fillStyle = "#2fff00";
+    const textWidth = ctx.measureText(prediction.class).width;
+    const textHeight = parseInt(font, 10);
+    // draw top left rectangle
+    ctx.fillRect(x, y, textWidth + 10, textHeight + 10);
+    // draw bottom left rectangle
+    ctx.fillRect(x, y + height - textHeight, textWidth + 15, textHeight + 10);
+
+    // Draw the text last to ensure it's on top.
+    ctx.fillStyle = "#000000";
+    ctx.fillText(prediction.class, x, y);
+    ctx.fillText(prediction.score.toFixed(2), x, y + height - textHeight);
+  });
 }
