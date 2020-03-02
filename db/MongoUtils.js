69393c5ff4da13d7b62f2a596017f53e86a43ad6
Diego Riveros <df.riveros11@uniandes.edu.co>
2020-03-02 08:31:35 -0500

    aqui vamos

--- db/MongoUtils.js
+++ db/MongoUtils.js
@@ -23,7 +23,7 @@ function MongoUtils() {
     console.log("Trying to connect");
     let url;
     if (user === undefined) {
-      url = process.env.MONGODB_URI;
+      url = process.env.MONGODB_URI + "?retryWrites=false";
     } else {
       url = `mongodb://${user}:${pwd}@${hostname}:${port}`;
     }
